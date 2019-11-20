862070d7c4e313068925f1e7027cc82934614daa
Dheeraj <dheeraj98reddy@gmail.com>
2019-11-20 20:51:17 +0530

    WIP: Add alloc definitions

--- include/shadesmar/allocator.h
+++ include/shadesmar/allocator.h
@@ -6,27 +6,50 @@
 #define SHADESMAR_ALLOCATOR_H
 
 namespace shm {
+struct MemoryBlock {
+  uint8_t *addr;
+  size_t size;
+};
+
+struct Info {
+  uint8_t init;
+  size_t rem_size;
+
+  MemoryBlock *blocks;
+};
+
+/*
+ * Assume that the underlying shared memory needs to be broken
+ * into roughly equal sized segments. We allocate an array at the
+ * start of the memory block for keeping track of block information.
+ * Every time we allocate any memory, we add a new Node to our list.
+ * We align to 16 bytes for the underlying memory space.
+ */
 class Allocator {
 public:
-  Allocator(uint8_t *raw_buffer);
+  Allocator(uint8_t *raw_buffer, size_t);
   uint8_t *malloc(size_t size);
   void free(uint8_t *ptr);
 
-  size_t get_offset(uint8_t *addr);
-  uint8_t *set_offset(size_t offset);
+  size_t get_offset(uint8_t *addr) const;
+  uint8_t *set_offset(size_t offset) const;
 
 private:
   uint8_t *raw_buffer_;
+  size_t total_size_;
 };
 
-Allocator::Allocator(const uint8_t *raw_buffer) : raw_buffer_(raw_buffer) {}
+Allocator::Allocator(uint8_t *raw_buffer, size_t total_size)
+    : raw_buffer_(raw_buffer), total_size_(total_size) {}
 
 uint8_t *Allocator::malloc(size_t size) { return nullptr; }
 
 void Allocator::free(uint8_t *ptr) { return; }
 
-size_t Allocator::get_offset(uint8_t *addr) { return addr - raw_buffer_; }
+size_t Allocator::get_offset(uint8_t *addr) const { return addr - raw_buffer_; }
 
-uint8_t *Allocator::set_offset(size_t offset) { return raw_buffer_ + offset; }
+uint8_t *Allocator::set_offset(size_t offset) const {
+  return raw_buffer_ + offset;
+}
 } // namespace shm
 #endif // SHADESMAR_ALLOCATOR_H
