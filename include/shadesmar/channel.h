b89c7c2ae6fd1630a7d79a498d45604499515306
Dheeraj <dheeraj98reddy@gmail.com>
2019-12-19 14:35:58 +0530

    Merge

--- include/shadesmar/channel.h
+++ include/shadesmar/channel.h
@@ -14,15 +14,9 @@
 
 #include <msgpack.hpp>
 
-<<<<<<< HEAD
-#include <shadesmar/ipc_lock.h>
-#include <shadesmar/macros.h>
-#include <shadesmar/memory.h>
-=======
 #include <shadesmar/macros.h>
 #include <shadesmar/memory.h>
 #include <shadesmar/robust_lock.h>
->>>>>>> 3e891103ea7b8f9a3bddb0713b81f9a2808b7176
 #include <shadesmar/tmp.h>
 
 namespace shm {
