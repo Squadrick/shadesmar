dfee424668fb2d8e3fd1cf786a963f4f6e8ffa9b
Dheeraj <dheeraj98reddy@gmail.com>
2020-01-19 15:02:50 +0530

    Add rpc server and client

---
+++ src/rpc_client.cpp
@@ -0,0 +1,22 @@
+//
+// Created by squadrick on 19/01/20.
+//
+
+#include <random>
+
+#include <shadesmar/client.h>
+
+int main() {
+  shm::rpc::FunctionCaller fmadd_fn("fmadd");
+
+  std::random_device device;
+  std::mt19937 generator(device());
+
+  std::uniform_real_distribution<float> dist(-1.0f, 1.0f);
+
+  float a = dist(generator);
+  float b = dist(generator);
+  float c = dist(generator);
+
+  std::cout << fmadd_fn(a, b, c).as<float>() << std::endl;
+}
---
+++ src/rpc_server.cpp
@@ -0,0 +1,15 @@
+//
+// Created by squadrick on 19/01/20.
+//
+
+#include <shadesmar/server.h>
+
+float fmadd(float a, float b, float c) { return a * b + c; }
+
+int main() {
+  shm::rpc::Function<float(float, float, float)> fmadd_rpc("fmadd", fmadd);
+
+  while (true) {
+    fmadd_rpc.serveOnce();
+  }
+}
