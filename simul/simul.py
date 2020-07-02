316b0a674810470f7b04d3d0ad2b4789d26d95a3
Dheeraj <dheeraj98reddy@gmail.com>
2020-07-03 02:55:33 +0530

    remove header guards

--- simul/simul.py
+++ simul/simul.py
@@ -41,6 +41,34 @@ def read_file(filename):
   return text_lines
 
 
+def remove_guards(code):
+  # [str]
+  guard_stack = []
+  guard_names = []
+  del_lines = []
+  for lno, line in enumerate(code):
+    if line.startswith("#if"):
+      guard_stack.append(lno)
+    if line.startswith("#endif"):
+      if_line = code[guard_stack[-1]]
+      if "#ifndef INCLUDE" in if_line:
+        del_lines.append(guard_stack[-1])
+        del_lines.append(lno)
+        guard_names.append(if_line[len("#ifndef "):-1])
+      guard_stack.pop()
+
+  for lno, line in enumerate(code):
+    for gname in guard_names:
+      if line.startswith("#define " + gname):
+        del_lines.append(lno)
+
+  for index in sorted(del_lines, reverse=True):
+    if "INCLUDE" in code[index]:
+      del code[index]
+
+  return code
+
+
 def crawl_src_folder(folder):
   return [path for path in Path(folder).rglob('*.h')]
 
@@ -59,7 +87,8 @@ def init_graph(source_files):
 def get_filename_text_dict(source_files):
   ft_dict = {}
   for source_file in source_files:
-    ft_dict[get_include_token(source_file)] = read_file(source_file)
+    code_with_guards = read_file(source_file)
+    ft_dict[get_include_token(source_file)] = remove_guards(code_with_guards)
   return ft_dict
 
 
