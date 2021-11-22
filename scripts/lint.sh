be782a0ead538ea74463947a2ca17c94124ab514
Dheeraj R Reddy <dheeraj98reddy@gmail.com>
2021-11-22 23:35:30 +0530

    [CI] Add release to lint exclusions

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,6 +4,7 @@ cpplint \
   --filter=-build/c++11,-build/include_subdir,-build/include_order \
   --exclude="include/catch.hpp" \
   --exclude="include/shadesmar.h" \
+  --exclude="release/shadesmar.h" \
   --exclude="build/*" \
   --exclude="cmake-build*" \
   --recursive .
